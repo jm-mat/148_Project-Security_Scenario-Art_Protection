ab681a1840fc005c70bc4e99ffc2b668b03fe23e
jm-mat <janmichael.maturan@sjsu.edu>
2026-05-05 11:00:12 -0700

    Update BAS

--- scrapers/fast_repeater_bot.py
+++ scrapers/fast_repeater_bot.py
@@ -5,7 +5,7 @@ import random
 import time
 import os
 
-BASE = "http://127.0.0.1:5001"
+BASE = "http://10.0.0.1:5001"   # Flask server running on host 'srv' in Mininet
 FAKE_IP = os.getenv("FAKE_IP", "127.0.0.1")
 
 PAGES = ["/", "/api/artworks", "/api/artwork/1", "/api/artwork/2"]
