7fb8101f4ccbf359fb0aface843b88ebb7178be5
jm-mat <janmichael.maturan@sjsu.edu>
2026-05-10 21:25:49 -0700

    Create topology1_mappings.py

---
+++ flask_app/topologies/topology1_mappings.py
@@ -0,0 +1,18 @@
+SCRAPER_NAMES = {
+    "10.0.0.2": "CasualHuman",
+    "10.0.0.3": "MultiHuman",
+    "10.0.0.4": "ExpertHuman",
+    "10.0.0.5": "FastRepeaterBot",
+    "10.0.0.6": "EnumeratorBot",
+    "10.0.0.7": "RandomWalkerBot",
+    "10.0.0.8": "AdaptiveBot",
+    "10.0.0.9": "StrategicBot",
+    "10.0.0.10": "LoadBot-1",
+    "10.0.0.11": "LoadBot-2",
+    "10.0.0.12": "LoadBot-3",
+    "10.0.0.13": "LoadBot-4"
+}
+
+BOT_CLUSTERS = {
+    "load_cluster": ["10.0.0.10", "10.0.0.11", "10.0.0.12", "10.0.0.13"]
+}
